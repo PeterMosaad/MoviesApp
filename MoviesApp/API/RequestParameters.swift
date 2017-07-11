7eba2e42e5dde4fa92b91904bf5bef18e480e6b8
Peter Mosaad <peter.mosaad@C02R110NFVH6.local>
2017-07-11 21:39:47 +0200

    Adding SearchMovieRequestParameters

--- MoviesApp/API/RequestParameters.swift
+++ MoviesApp/API/RequestParameters.swift
@@ -12,6 +12,7 @@ let defaultAPIKey = "2696829a81b1b5827d515ff121700838"
 
 enum QueryKeys {
   static let apiKey = "api_key"
+  static let searchQuery = "query"
 }
 
 class RequestParameters {
@@ -24,6 +25,21 @@ class RequestParameters {
   func queryRepresentation() -> [String: String?] {
     var queryParams = [String: String]()
     queryParams[QueryKeys.apiKey] = apiKey
+    return queryParams
+  }
+}
+
+class SearchMovieRequestParameters : RequestParameters {
+  let searchQuery: String
+
+  init(searchQuery: String) {
+    self.searchQuery = searchQuery
+    super.init()
+  }
+
+  override func queryRepresentation() -> [String: String?] {
+    var queryParams = super.queryRepresentation()
+    queryParams[QueryKeys.searchQuery] = searchQuery
     return queryParams
   }
 }
