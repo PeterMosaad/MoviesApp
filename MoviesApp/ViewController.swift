9a62bf0c6feb83060d0c3001d15f30711b6a8189
Peter Mosaad <peter.mosaad@C02R110NFVH6.local>
2017-07-11 20:43:50 +0200

    initial Commit for the Project (auto generated Xcode project)

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Peter Mosaad on 7/11/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
