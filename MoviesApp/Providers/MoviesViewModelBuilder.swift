6e7ac40493c9aa3dd133e9b6af3860791dc5fa87
Peter Mosaad <peter.mosaad@C02R110NFVH6.fritz.box>
2017-07-12 03:59:08 +0200

    Add ViewModels and Poster URL builder

---
+++ MoviesApp/Providers/MoviesViewModelBuilder.swift
@@ -0,0 +1,41 @@
+//
+//  ImagesURLBuilder.swift
+//  MoviesApp
+//
+//  Created by Peter Mosaad on 7/12/17.
+//
+//
+
+import Foundation
+
+protocol MoviesPosterURLBuilder {
+  func fullURLFor(posterPath: String) -> String
+}
+
+struct MovieViewModel {
+  let name: String
+  let releaseDate: String
+  let overView: String
+  let posterURL: String
+
+  init(movie: MovieViewModel, posterURLBuilder: MoviesPosterURLBuilder) {
+    name = movie.name
+    releaseDate = movie.releaseDate
+    overView = movie.overView
+    posterURL = posterURLBuilder.fullURLFor(posterPath: movie.posterURL)
+  }
+}
+
+class MoviePosterURLBuilder: MoviesPosterURLBuilder {
+  let baseImagesURL: String
+  let preferedSize: Int
+
+  init(baseURL: String, preferedSize: Int) {
+    self.baseImagesURL = baseURL
+    self.preferedSize = preferedSize
+  }
+
+  func fullURLFor(posterPath: String) -> String {
+    return baseImagesURL + "w" + String(preferedSize) + "/" + posterPath
+  }
+}
