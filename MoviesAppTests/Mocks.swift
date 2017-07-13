b6cae9287e0db0de1849909263aa19c4766b7aa1
petermosaad <petermosaad@gmail.com>
2017-07-13 18:37:57 +0200

    Adding Tests for MoviesProvider

--- MoviesAppTests/Mocks.swift
+++ MoviesAppTests/Mocks.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+import Siesta
+@testable import MoviesApp
+
+class CachingProviderMock: CachingProvider {
+  var cacheObjectCallsCount = 0
+  var loadObjectCallsCount = 0
+  var persistenceSimulator = [String: Any]()
+
+  func cache(object: Any, forKey key: String) {
+    cacheObjectCallsCount += 1
+    persistenceSimulator.updateValue(object, forKey: key)
+  }
+
+  func load(objectForKey key: String) -> Any? {
+    loadObjectCallsCount += 1
+    return persistenceSimulator[key]
+  }
+}
+
+struct NetworkStub: NetworkingProvider {
+  var responses: [String:ResponseStub] = [:]
+
+  func startRequest(
+    _ request: URLRequest,
+    completion: @escaping RequestNetworkingCompletionCallback)
+    -> RequestNetworking {
+      let responseStub = responses[request.url!.path]
+      let statusCode = 200
+      let response = HTTPURLResponse(
+        url: request.url!,
+        statusCode: statusCode,
+        httpVersion: "HTTP/1.1",
+        headerFields: [:])
+
+      completion(response, responseStub?.data, nil)
+      return RequestStub()
+  }
+}
+
+struct ResponseStub {
+  let contentType: String = "*/json"
+  let data = Data()
+}
+
+struct RequestStub: RequestNetworking {
+  func cancel() { }
+
+  var transferMetrics: RequestTransferMetrics {
+    return RequestTransferMetrics(
+      requestBytesSent: 0,
+      requestBytesTotal: nil,
+      responseBytesReceived: 0,
+      responseBytesTotal: nil)
+  }
+}
+
+class MockService: Service {
+  init(responseToBe: Any) {
+    super.init(baseURL: "http://test.ing",
+               useDefaultTransformers: true,
+               networking: NetworkStub())
+
+    configureTransformer("") { (entity: Entity<Any>) -> Any in
+      return responseToBe
+    }
+  }
+}
--- MoviesAppTests/MoviesProviderTests.swift
+++ MoviesAppTests/MoviesProviderTests.swift
@@ -7,29 +7,144 @@
 //
 
 import XCTest
+import Siesta
+@testable import MoviesApp
+
+class MoviesServiceClientMock: SearchMoviesServiceClient {
+
+  let shouldSuccess: Bool
+  var searchMoviesCallsCount = 0
+  let tempMovie = Movie()
+  init(toSuccess: Bool = true) {
+    shouldSuccess = toSuccess
+  }
+  func searchMovies(_ requestParams: SearchMovieRequestParameters) -> Resource {
+    searchMoviesCallsCount += 1
+    var service: Service
+    if shouldSuccess {
+      service = MockService(responseToBe: [tempMovie])
+    } else {
+      service = MockService(responseToBe: [])
+    }
+    return service.resource("")
+  }
+}
+
+extension Movie {
+  init() {
+    name = "name"
+    releaseDate = "releaseDate"
+    overView = "overView"
+    posterURL = "posterURL"
+  }
+}
 
 class MoviesProviderTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+  var searchClient: MoviesServiceClientMock!
+  var provider: MoviesProvider!
+  var cachingProvider: CachingProviderMock!
+  let maxCacheSize = 1
+
+  override func setUp() {
+    searchClient = MoviesServiceClientMock()
+    cachingProvider = CachingProviderMock()
+    provider = MoviesManager(searchClient: searchClient, cachingProvider: cachingProvider, maxCacheSize: maxCacheSize)
+  }
+
+  func testCallingServiceWhenSearchRequested() {
+    provider.searchMovies(query: "query") { (_, _) in }
+    XCTAssert(searchClient.searchMoviesCallsCount > 0, "EXPECTED: Search client is called")
+  }
+
+  func testSuccessQueriesAreCached() {
+
+    let test = expectation(description: "Request")
+    provider.searchMovies(query: "query") { (moviesList, error) in
+      XCTAssert(self.cachingProvider.cacheObjectCallsCount > 0, "EXPECTED: Caching Provider is called")
+      test.fulfill()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+
+  func testFailedQueriesAreNotCached() {
+
+    searchClient = MoviesServiceClientMock(toSuccess: false)
+    provider = MoviesManager(searchClient: searchClient, cachingProvider: cachingProvider, maxCacheSize: maxCacheSize)
+    let test = expectation(description: "Request")
+    provider.searchMovies(query: "query") { (moviesList, error) in
+      XCTAssert(self.cachingProvider.cacheObjectCallsCount == 0, "EXPECTED: Caching Provider is not called")
+      test.fulfill()
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
     }
-    
+  }
+
+  func testGetSuccessfulSearchQueries() {
+    let query = "query"
+    let expectedQueries = ["query"]
+    let test = expectation(description: "Request")
+    provider.searchMovies(query: query) { (moviesList, error) in
+      let queries = self.provider.getLastSuccessfulSearchQueries()
+      XCTAssert(queries == expectedQueries, "EXPECTED: LoadedQueries to be \(String(describing: expectedQueries))")
+      test.fulfill()
+    }
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
+    }
+  }
+
+  func testCachingRespectMaxSize() {
+
+    let test = expectation(description: "Request")
+    provider.searchMovies(query: "query") { (moviesList, error) in
+      self.provider.searchMovies(query: "query2") { (moviesList, error) in
+        let queries = self.provider.getLastSuccessfulSearchQueries()
+        XCTAssert(queries.count <= self.maxCacheSize, "EXPECTED: Caching Only results according to max cache size")
+        test.fulfill()
+      }
+    }
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
+    }
+  }
+
+  func testCachingRemovesDuplicate() {
+
+    let test = expectation(description: "Request")
+    provider = MoviesManager(searchClient: searchClient, cachingProvider: cachingProvider, maxCacheSize: 2)
+    provider.searchMovies(query: "query") { (moviesList, error) in
+      self.provider.searchMovies(query: "query") { (moviesList, error) in
+        let queries = self.provider.getLastSuccessfulSearchQueries()
+        let queriesSet = Set(queries)// To remove Duplicates
+        XCTAssert(queries.count <= queriesSet.count, "EXPECTED: Caching removes duplicate results")
+        test.fulfill()
+      }
+    }
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
+    }
+  }
+
+  func testCachingInFIFO() {
+
+    let query1 = "query1"
+    let query2 = "query2"
+    let test = expectation(description: "Request")
+    provider = MoviesManager(searchClient: searchClient, cachingProvider: cachingProvider, maxCacheSize: 2)
+    provider.searchMovies(query: query1) { (moviesList, error) in
+      self.provider.searchMovies(query: query2) { (moviesList, error) in
+        let queries = self.provider.getLastSuccessfulSearchQueries()
+        XCTAssert(queries.first == query2, "EXPECTED: Caching should be in order FIFO")
+        test.fulfill()
+      }
+    }
+    waitForExpectations(timeout: 5) {_ in
+      print("error")
+    }
+  }
+
 }
